c3792da86939ca1c62d5396b195c876bf6f1d120
aezazali1997 <aezazali.pro@gmail.com>
2023-04-20 18:02:05 +0500

    added middleware

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -1,34 +1,35 @@
 const express = require('express');
 const router = express.Router();
+const authMiddleware = require('../middlewares/auth');
 const bookController = require('../controllers/bookController');
 
 // CREATE - Create a new book
-router.post('/', bookController.create);
+router.post('/', authMiddleware, bookController.create);
 
 // READ - Get all books
-router.get('/', bookController.getAll);
+router.get('/', authMiddleware, bookController.getAll);
 // READ - Get all books by shopId
 
 // READ - Get a book by ID
-router.get('/:id', bookController.getById);
+router.get('/:id', authMiddleware, bookController.getById);
 
 // UPDATE - Update a book by ID
-router.put('/:id', bookController.updateById);
+router.put('/:id', authMiddleware, bookController.updateById);
 
 // DELETE - Delete a book by ID
-router.delete('/:id', bookController.deleteById);
+router.delete('/:id', authMiddleware, bookController.deleteById);
 
 // Get the top rated books
 
-router.post('/top-rated', bookController.getTopRated);
+router.post('/top-rated', authMiddleware, bookController.getTopRated);
 
 // Get the books which are on sale
 
-router.post('/on-sale', bookController.getBooksOnSale);
+router.post('/on-sale', authMiddleware, bookController.getBooksOnSale);
 
 // Get Books by Category 
 
-router.get('/by-category/:categoryId', bookController.getBooksByCategory);
+router.get('/by-category', authMiddleware, bookController.getBooksByCategory);
 
 
 
